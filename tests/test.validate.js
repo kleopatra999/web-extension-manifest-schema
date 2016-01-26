5f3392a5f4e23e95bdcc251b5592abeb50acab19
Stuart Colville <scolville@mozilla.com>
2016-01-26 18:57:12 +0000

    Hook up version format

--- tests/test.validate.js
+++ tests/test.validate.js
@@ -6,7 +6,7 @@ describe('Schema', () => {
 
   it('should be valid against the extension schema', () => {
     var isValid = validate(validManifest);
-    assert.ok(isValid);
+    assert.ok(isValid, validate.errors);
   });
 
   it('should be invalid due to old manifest_version', () => {
@@ -14,7 +14,17 @@ describe('Schema', () => {
       manifest_version: 1,
     }));
     assert.notOk(isValid, validate.errors);
+    assert.equal(validate.errors[0].dataPath, '.manifest_version');
     assert.equal(validate.errors.length, 1);
+  });
+
+  it('should be invalid due to invalid version', () => {
+    var isValid = validate(Object.assign({}, validManifest, {
+      version: '01',
+    }));
+    assert.equal(validate.errors.length, 1);
+    assert.equal(validate.errors[0].dataPath, '.version');
+    assert.notOk(isValid, validate.errors);
   });
 
 });
