66d85d4bbc2bdafc66ca477d9060532cd4c47095
Stuart Colville <scolville@mozilla.com>
2016-01-26 18:31:34 +0000

    Add format for version string

---
+++ src/formats.js
@@ -0,0 +1,28 @@
+const VALIDNUMRX = /^[0-9]{1,5}$/;
+
+
+export function isValidVersionString(version) {
+  var parts = version.split('.');
+  if (parts.length > 4) {
+    return false;
+  }
+  for (var part of parts) {
+    // Must start as a string.
+    if (typeof part !== 'string') {
+      return false;
+    }
+    // Leading or multiple zeros not allowed.
+    if (part.startsWith('0') && part.length > 1) {
+      return false;
+    }
+    // Disallow things like 123e5 which parseInt will convert.
+    if (!VALIDNUMRX.test(part)) {
+      return false;
+    }
+    part = parseInt(part, 10);
+    if (typeof part !== 'number' && part !== NaN || part < 0 || part > 65535) {
+      return false;
+    }
+  }
+  return true;
+}
